4baa190f4b1d1b8f882ccc15c21e100ae132cd93
Volodymyr Rusynov <volodymyr.rusynov@repairify.com>
2022-10-04 23:42:11 +0300

    Use FlatList for TableScreen instead of ScrollView to boost rendering performance

--- src/TableScreen.tsx
+++ src/TableScreen.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {DataTable} from 'react-native-paper';
-import {ScrollView, StyleSheet} from 'react-native';
+import {FlatList, ScrollView, StyleSheet} from 'react-native';
 import {queryDataFromTable} from './db';
-
+import {TableRow} from './TableRow';
 interface TableScreenProps {
   tbl_name: string;
 }
@@ -16,41 +16,27 @@ export const TableScreen = ({tbl_name}: TableScreenProps) => {
     })();
   }, [tbl_name]);
 
-  console.log(data);
+  // console.log(data);
 
   if (data.length > 0) {
     const headers = Object.keys(data[0]);
 
     return (
       <ScrollView horizontal>
-        <ScrollView>
-          <DataTable>
-            <DataTable.Header>
-              {headers.map((header, index) => (
-                <DataTable.Title
-                  style={styles.cell}
-                  key={String(header) + index}>
-                  {header}
-                </DataTable.Title>
-              ))}
-            </DataTable.Header>
-            {data.map((row: object, index: number) => {
-              const values = Object.values(row);
-
-              return (
-                <DataTable.Row key={index}>
-                  {values.map((value, idx: number) => (
-                    <DataTable.Cell
-                      style={styles.cell}
-                      key={String(value) + index + idx}>
-                      {String(value).slice(0, 10)}
-                    </DataTable.Cell>
-                  ))}
-                </DataTable.Row>
-              );
-            })}
-          </DataTable>
-        </ScrollView>
+        <DataTable>
+          <DataTable.Header>
+            {headers.map((header, index) => (
+              <DataTable.Title style={styles.cell} key={String(header) + index}>
+                {header}
+              </DataTable.Title>
+            ))}
+          </DataTable.Header>
+          <FlatList
+            data={data}
+            initialNumToRender={20}
+            renderItem={({item}) => <TableRow data={item} />}
+          />
+        </DataTable>
       </ScrollView>
     );
   } else {
